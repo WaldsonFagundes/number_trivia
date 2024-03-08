48747de035c8cc0fba9a64719456babcb426e22c
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-03-08 10:22:15 -0300

    created setUp for test

--- test/feature/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
+++ test/feature/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -1,5 +1,20 @@
 import 'package:trivia/features/number_trivia/domain/repositories/repositories.dart';
+
 import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
+
+void main(){
+
+  GetConcreteNumberTrivia usecase:
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+
+  });
+
+}
