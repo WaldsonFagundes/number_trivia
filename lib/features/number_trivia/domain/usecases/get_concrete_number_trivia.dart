dbc4d577ea8a10b2cbf3d4d959a24797652ba822
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-03-08 11:06:03 -0300

    added packege faker, added to GetConcreteNumberTrivia the function .execute, and ensure that get trivia for the number from the repository

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:trivia/features/number_trivia/domain/entities/entities.dart';
+
 import '../repositories/repositories.dart';
+import '../../../../core/core.dart';
 
 class GetConcreteNumberTrivia {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
 }
--- test/feature/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
+++ test/feature/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -1,20 +1,38 @@
-import 'package:trivia/features/number_trivia/domain/repositories/repositories.dart';
-
+import 'package:dartz/dartz.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:faker/faker.dart';
+
+import 'package:trivia/features/number_trivia/domain/entities/entities.dart';
+import 'package:trivia/features/number_trivia/domain/repositories/repositories.dart';
+import 'package:trivia/features/number_trivia/domain/usecases/usecases.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
-void main(){
-
-  GetConcreteNumberTrivia usecase:
-  MockNumberTriviaRepository mockNumberTriviaRepository;
+void main() {
+ late GetConcreteNumberTrivia usecase;
+ late MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
-
   });
 
+  const tNumber = 1;
+  const tNumberTrivia = NumberTrivia(text: 'test', number: 1);
+  int anyNumber = faker.guid.random.integer(100000, min: 0);
+
+  test('Should get trivia for the number from the repository', () async {
+
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(anyNumber))
+        .thenAnswer((_) async => const Right(tNumberTrivia));
+
+    final result = await usecase.execute(number: tNumber);
+
+    expect(result, const Right(tNumberTrivia));
+    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
+
+  });
 }
