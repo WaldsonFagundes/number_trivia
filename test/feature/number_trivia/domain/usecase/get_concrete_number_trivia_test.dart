cf34d184fed8141bc48b81096d20ce16b2905e13
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-03-08 10:15:20 -0300

    created mock MockNumberTriviaRepository in get_concrete_number_trivia_test

---
+++ test/feature/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -0,0 +1,5 @@
+import 'package:trivia/features/number_trivia/domain/repositories/repositories.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
