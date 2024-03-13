f23749074283f39728e78a8ddc4c061304f4902f
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-03-13 17:07:43 -0300

    created a factory for NumberTriviaModel and guaranteed a valid return when the JSON is an integer

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -1,7 +1,10 @@
 import '../../domain/e_domain.dart';
 
 class NumberTriviaModel extends NumberTrivia {
-
   const NumberTriviaModel({required int number, required String text})
       : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(number: json['number'], text: json['text']);
+  }
 }
--- test/feature/number_trivia/data/models/number_trivia_model_test.dart
+++ test/feature/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,14 +1,27 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:trivia/features/number_trivia/data/e_data.dart';
 import 'package:trivia/features/number_trivia/domain/e_domain.dart';
 
-void main(){
-  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Test');
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test Text');
 
   test('Should be a subclass of NumberTrivia entity', () async {
-
     expect(tNumberTriviaModel, isA<NumberTrivia>());
   });
 
+  group('fromJson', () {
+    test('Should return a valid model when the JSON number is an integer',
+        () async {
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+
+      final result = NumberTriviaModel.fromJson(jsonMap);
+
+      expect(result, tNumberTriviaModel);
+    });
+  });
 }
