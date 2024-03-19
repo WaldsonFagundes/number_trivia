ff533e0ce592142a39806ff6e73b53daf1613636
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-03-19 09:14:19 -0300

    added NumberTriviaRepositoryImpl class

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:trivia/core/error/failures.dart';
+
+import '../../domain/e_domain.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    throw UnimplementedError();
+  }
+
+}
