46a38006ecf8a61b83c688d0b42e14c416734716
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-03-19 09:28:57 -0300

    created NumberTriviaRemoteDataSource contract

---
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -0,0 +1,14 @@
+import '../e_data.dart';
+
+abstract class NumberTriviaRemoteDataSource {
+  /// Calls the http://numbersapi.com/{number} endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
+
+  /// Calls the http://numbersapi.com/random endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<NumberTriviaModel> getRandomNumberTrivia();
+
+}
