87a3622204f044183ec9318e96db9f0241da48ec
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-03-25 17:42:25 -0300

    ensure the call forward to InternetConnectionChecker.hasConnection

---
+++ test/core/error/network_info_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:trivia/core/e_core.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import 'network_info_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<InternetConnectionChecker>()])
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockInternetConnectionChecker mockConectionChecker;
+
+  setUp(() {
+    mockConectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockConectionChecker);
+  });
+
+  group('isConnected', () {
+    test("Should forward the call to InternetConnectionChecker.hasConnection",
+        () async {
+      final tHasConnectionFuture = Future.value(true);
+
+      when(mockConectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+
+      final result = networkInfo.isConnected;
+
+      verify(mockConectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
