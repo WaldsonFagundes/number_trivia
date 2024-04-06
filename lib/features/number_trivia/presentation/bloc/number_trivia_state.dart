ab63050a72328da10186b20c77743524c84e42e6
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2024-04-06 16:23:21 -0300

    added requireds state to number_trivia_state

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:trivia/features/number_trivia/domain/e_domain.dart';
 
 abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
@@ -7,4 +8,23 @@ abstract class NumberTriviaState extends Equatable {
   List<Object?> get props => [];
 }
 
-class InitialNumberTriviaState extends NumberTriviaState {}
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  const Loaded({required this.trivia});
+
+  @override
+  List<Object?> get props => [trivia];
+}
+class Error extends NumberTriviaState {
+  final String message;
+
+  const Error({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
